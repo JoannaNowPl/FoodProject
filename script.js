77f36baa9e8a17e38af714d3808ac4ce481d8165
Joanna Nowakowska <jzolnow@gmail.com>
2022-08-02 18:13:50 +0200

    Added nutrition

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "a30370d761mshe2f4a36a878186bp14d136jsn7d1a9ba8b0e7",
+    "X-RapidAPI-Key": "12c9c6d2fdmsh353138efd4d68d7p189610jsn6b8bd151a6c6",
     "X-RapidAPI-Host": "tasty.p.rapidapi.com",
   },
 };
@@ -34,18 +34,18 @@ const onSuccessCallback = (data) => {
       recipeDiv.className = "recipeDiv";
 
       const recipeName = document.createElement("button");
-      recipeName.innerText = name;
+      recipeName.innerHTML = name;
       recipeName.className = "recipeName";
       recipeName.onclick = () => {
         getRecipeDetails(id);
       };
      
       const tagsTable=[];
-      for(const el of tags){tagsTable.push(el.name)};
+      for(const el of tags){tagsTable.push(el.display_name)};
     
       const tagsDiv = document.createElement("div");
       tagsDiv.className="tagsDiv";
-      tagsDiv.innerText=`#${tagsTable.join(" #")}`;
+      tagsDiv.innerHTML=`#${tagsTable.join(" #")}`;
 
       const recipeImage = document.createElement("img");
       recipeImage.src = thumbnail_url;
@@ -80,20 +80,52 @@ const onErrorCallback = (error) => console.log(error);
 loadData(onSuccessCallback, onErrorCallback);
 
 const showInstructions = (data) => {
-  const instructionsDiv = document.querySelector(".instructions");
+  const instructionsDiv = document.querySelector(".instructionsDiv");
+
+  const instructions = document.createElement("div");
+  instructions.className = "instructions";
+
+  instructionsDiv.append(instructions);
 
   const title = document.createElement("h3");
   title.innerHTML = data.name;
 
-  const instructions = document.createElement("h4");
-  instructions.innerText = "Instructions:";
+  const ingredientsHeader = document.createElement("h4");
+  ingredientsHeader.innerHTML = "Ingredients:"; 
 
-  instructionsDiv.append(title, instructions);
+  instructions.append(title, ingredientsHeader);
 
-  for (const instructions of data.instructions) {
-    const { position, display_text } = instructions;
+  const componentsTable = data.sections[0].components;
+  
+
+  for (const element of componentsTable) {
+    const {raw_text} = element;
+    
+    const ingredient = document.createElement("p");
+    ingredient.innerHTML = `${raw_text}`;
+    instructions.append(ingredient);
+  }
+
+  const instructionsHeader = document.createElement("h4");
+  instructionsHeader.innerHTML = "Instructions:";
+
+  instructions.append(instructionsHeader);
+
+  for (const step of data.instructions) {
+    const { position, display_text } = step;
     const instructionsStep = document.createElement("p");
     instructionsStep.innerHTML = `${position}. ${display_text}`;
-    instructionsDiv.append(instructionsStep);
+    instructions.append(instructionsStep);
   }
+
+  const nutritionHeader = document.createElement("h4");
+  nutritionHeader.innerHTML = "Nutrition:";
+  instructions.append(nutritionHeader);
+
+  for (const nutriKey in data.nutrition){     
+     if(nutriKey==="updated_at") continue;   
+    const nutriP = document.createElement("p");      
+    nutriP.innerHTML=`${nutriKey}: ${data.nutrition[nutriKey]}`;    
+    instructions.append(nutriP);
+    };
 };
